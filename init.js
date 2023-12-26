a7056c7e3acfeaa9837e3495a161080bdf3ad4ab
Morgen-Henry <122406698+Morgen-Henry@users.noreply.github.com>
2023-12-26 15:21:36 -0500

    overworld message received?

--- init.js
+++ init.js
@@ -1,3 +1,6 @@
 (function () {
-    console.log("It's working!")
+    const overworld = new Overworld({
+        element: document.querySelector(".game-container")
+    });
+    overworld.init();
 })();
